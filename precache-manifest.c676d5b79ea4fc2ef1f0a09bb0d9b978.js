bb02919229c7960b9c39ef6370ea427988f9796a
Inna Sorochynska <innasoro@gmail.com>
2021-04-19 15:51:11 +0300

    Updates

---
+++ precache-manifest.c676d5b79ea4fc2ef1f0a09bb0d9b978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0b55c4c1e53f571e14954d1a0206a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "6606b2c2658e026809da",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/js/2.28065652.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.28065652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6606b2c2658e026809da",
+    "url": "./static/js/main.37664e25.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
